d0f888fe58b2df4bd7d9e39260a57475799dc62b
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-11 23:37:22 -0300

    criado funcoes

---
+++ script.js
@@ -0,0 +1,25 @@
+function insert(num){
+    let numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + num;  
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function apagar(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length - 1);
+}
+
+function calcular(){
+    let resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado);
+    }else{
+        alert('Nada para calcular!');
+    }
+}
+
+/* use apenas o eval() em funções internas do codigo,
+Jamais use o eval() quando precisar de inputs do servidor*/
